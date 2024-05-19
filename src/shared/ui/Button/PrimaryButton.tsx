48950c0685053557455086bf42ea540f1b551582
cgw-notebook <mangang0713@gmail.com>
2024-05-19 20:21:44 +0900

    #2 feat: PrimaryButton 생성

---
+++ src/shared/ui/Button/PrimaryButton.tsx
@@ -0,0 +1,54 @@
+import { PropsWithChildren } from "react";
+
+interface ButtonProps extends React.ComponentProps<'button'> {
+  size : string;
+  onClick?: () => void;
+  disabled?: boolean;
+}
+
+const PrimaryButton = ({size, onClick, disabled = false, children} : PropsWithChildren<ButtonProps>) => {
+
+  let buttonClass = `flex py-6  justify-center items-center gap-[10px] flex-shrink-0 rounded-lg whitespace-nowrap`;
+
+  switch (size) {
+    case 'L' : 
+      buttonClass += ' w-[640px] h-[65px]';
+      break;
+    case 'M' :
+      buttonClass += ' w-[440px] h-[55px]';
+      break;
+    case 'S' :
+      buttonClass += ' w-[335px] h-[50px]';
+      break;
+  }
+
+  if (disabled) {
+    buttonClass += ' bg-disabled text-gray-6E';
+  } else {
+    buttonClass += ' bg-main-gradation text-white';
+  }
+
+
+  
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={onClick}
+      disabled={disabled}  
+    >
+      {children}
+    </button>
+  );
+};
+
+/* 사용법 
+크기를 L, M, S로 두고, disabled의 경우에는 boolean 값으로 disabled를 설정하시면 됩니다.
+onClick Prop을 활용하여 다음 동작을 결정하시면 됩니다.
+      <PrimaryButton size="S" disabled={true}>
+        스몰 비활성화
+      </PrimaryButton>
+  와 같이 사용하시면 children props로 값이 전달됩니다. 
+*/
+
+export default PrimaryButton;

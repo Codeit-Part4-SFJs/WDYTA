7a8f4e1d45f4aee13df50c082da82284d3f0d621
yeona <aya813@naver.com>
2024-05-21 02:42:06 +0900

    #1 feat: TextFieldInput 구현

---
+++ src/shared/ui/Input/TextField/index.tsx
@@ -0,0 +1,11 @@
+const TextFieldInput = () => {
+  return (
+    <input
+      className="w-[335px] md:w-[360px] lg:w-[440px] h-[55px] md:h-[60px] lg:h-[70px] px-[20px] py-[23px] rounded-[8px] border border-solid border-gray-35 bg-black-25 text-gray-F1 text-[14px] md:text-[16px] font-normal placeholder-gray-6E focus:border-main-blue focus:outline-none"
+      type="text"
+      placeholder="상품명 (상품 등록 여부를 확인해 주세요)"
+    />
+  );
+};
+
+export default TextFieldInput;

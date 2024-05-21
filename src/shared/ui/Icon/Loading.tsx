807d676803aa88905d0620f22526e481cc597d53
hyunjun9788 <namhyunjun9788@gmail.com>
2024-05-22 01:13:40 +0900

    #9 fix: Loading 컴포넌트 color prop 추가

--- src/shared/ui/Icon/Loading.tsx
+++ src/shared/ui/Icon/Loading.tsx
@@ -3,16 +3,17 @@ import LoadingIcon from "../../../../public/icon/loading.svg";
 
 interface Loading {
   size: "S" | "L";
+  color: string;
   children: React.ReactNode;
 }
-const Loading = ({ size = "L", children }: Loading) => {
+const Loading = ({ size = "L", color, children }: Loading) => {
   const sizeClass = size === "S" ? "w-10 h-8" : "w-12 h-10";
   const fontClass =
     size === "S" ? "text-lg text-gray-6E" : "text-xl text-gray-6E";
 
   return (
     <div className="flex flex-col items-center gap-2">
-      <LoadingIcon className={`${sizeClass}`} />
+      <LoadingIcon className={`${sizeClass}`} color={color} />
       <p className={`${fontClass}`}>{children}</p>
     </div>
   );

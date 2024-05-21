9c871a128babdbdf91870bf25bf6afb6db9beff0
yeona <aya813@naver.com>
2024-05-21 20:04:31 +0900

    #1 refactor: 불필요한 style 속성 제거

--- src/shared/ui/Input/TextBox/index.tsx
+++ src/shared/ui/Input/TextBox/index.tsx
@@ -13,7 +13,7 @@ const TextBoxInput = () => {
       className={`relative w-[295px] md:w-[510px] lg:w-[540px] h-[120px] md:h-[160px] p-[20px] rounded-[8px] border border-solid ${isFocused ? "border-main-blue" : "border-gray-35"} bg-black-25`}
     >
       <textarea
-        className="box-border overflow-auto scrollbar-hide w-full h-[66px] md:h-[106px] bg-black-25 text-gray-F1 text-[14px] lg:text-[16px] font-normal placeholder-gray-6 resize-none focus:outline-none leading-[20px] lg:leading-[22px]"
+        className="scrollbar-hide w-full h-[66px] md:h-[106px] bg-black-25 text-gray-F1 text-[14px] lg:text-[16px] font-normal placeholder-gray-6 resize-none focus:outline-none leading-[20px] lg:leading-[22px]"
         placeholder="리뷰를 작성해 주세요"
         {...register("textarea")}
         maxLength={299}

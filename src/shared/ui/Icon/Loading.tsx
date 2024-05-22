a30f216c8825e5bf7eeb6a600fc39f1ecea0012d
hyunjun9788 <namhyunjun9788@gmail.com>
2024-05-22 16:04:26 +0900

    #9 fix: Loading 컴포넌트 반응형 로직 변경

--- src/shared/ui/Icon/Loading.tsx
+++ src/shared/ui/Icon/Loading.tsx
@@ -2,19 +2,21 @@ import Image from "next/image";
 import LoadingIcon from "../../../../public/icon/loading.svg";
 
 interface Loading {
-  size: "S" | "L";
+  iconSizeClass: string;
+  fontSizeClass: string;
   color: string;
   children: React.ReactNode;
 }
-const Loading = ({ size = "L", color, children }: Loading) => {
-  const sizeClass = size === "S" ? "w-10 h-8" : "w-12 h-10";
-  const fontClass =
-    size === "S" ? "text-lg text-gray-6E" : "text-xl text-gray-6E";
-
+const Loading = ({
+  iconSizeClass,
+  fontSizeClass,
+  color,
+  children,
+}: Loading) => {
   return (
     <div className="flex flex-col items-center gap-2">
-      <LoadingIcon className={`${sizeClass}`} color={color} />
-      <p className={`${fontClass}`}>{children}</p>
+      <LoadingIcon className={`${iconSizeClass}`} color={color} />
+      <p className={`${fontSizeClass} text-gray-6E`}>{children}</p>
     </div>
   );
 };

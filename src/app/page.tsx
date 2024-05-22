493445d48c0ae76d2a327e58a5e10ef7884652f7
linason-esc <linason7889@gmail.com>
2024-05-23 06:59:29 +0900

    [#14] feat: CompareChip 2개 UI 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,7 @@
 import CategoryChip from "@/shared/ui/Chip/CategoryChip";
 import CategoryFilterChip from "@/shared/ui/Chip/CategoryFilterChip";
+import CompareAChip from "@/shared/ui/Chip/CompareAChip";
+import CompareBChip from "@/shared/ui/Chip/CompareBChip";
 import RankingChip from "@/shared/ui/Chip/RankingChip";
 import ThumbsChip from "@/shared/ui/Chip/ThumbsChip";
 
@@ -15,6 +17,8 @@ export default function Home() {
       <CategoryFilterChip categoryID={1} />
       <CategoryFilterChip categoryID={0} />
       <ThumbsChip isLike={false} likeCount={99} />
+      <CompareAChip productName="IPhone 15 Pro MAX" />
+      <CompareBChip productName="Galaxy S22 Ultra" />
     </div>
   );
 }

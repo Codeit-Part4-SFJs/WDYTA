980114f8c74654bb620502475fe53047caf23583
Sumin <csmxx99@naver.com>
2024-05-23 00:36:21 +0900

    [#6] refactor: currentCategory slice 저장 공간 세션 스토리지로 변경

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,22 +1,21 @@
 import { create } from "zustand";
 import { CurrentCategorySlice } from "./storeType";
 import createCurrentCategorySlice from "./slices/createCurrentCategorySlice";
-import { devtools, persist } from "zustand/middleware";
+import { createJSONStorage, devtools, persist } from "zustand/middleware";
 
 type MyState = CurrentCategorySlice;
 
-const useBoundStore = create<MyState>()(
-  devtools(
-    persist(
-      (...a) => ({
-        ...createCurrentCategorySlice(...a),
+const useBoundStore = create<MyState>()((...a) => ({
+  ...devtools(
+    persist(createCurrentCategorySlice, {
+      name: "currentCategory",
+      storage: createJSONStorage(() => sessionStorage),
+      partialize: (state) => ({
+        currentCategory: state.currentCategory,
       }),
-      {
-        name: "BoundStore",
-        partialize: (state) => ({ currentCategory: state.currentCategory }),
-      }
-    )
-  )
-);
+    }),
+    { name: "CurrentCategorySlice" }
+  )(...a),
+}));
 
 export default useBoundStore;

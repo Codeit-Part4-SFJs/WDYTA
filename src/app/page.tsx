3712e615ecdacb1c322a086f3534f89cc671c1cb
linason-esc <linason7889@gmail.com>
2024-05-23 05:08:37 +0900

    [#3] feat: CategoryChip UI 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import CategoryChip from "@/shared/ui/Chip/CategoryChip";
 import RankingChip from "@/shared/ui/Chip/RankingChip";
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
       <RankingChip ranking={1} />
       <RankingChip ranking={2} />
       <RankingChip ranking={3} />
+      <CategoryChip categoryID={1} />
     </div>
   );
 }

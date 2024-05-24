636409c15a5fbcea053a5b5d068b12a59d6704a3
Sumin <csmxx99@naver.com>
2024-05-24 12:24:19 +0900

    [#29] chore: 클로즈 훅 공통화

---
+++ src/shared/@common/hooks/useClose.ts
@@ -0,0 +1,42 @@
+import React, { useEffect } from "react";
+
+/**
+ * 외부 영역을 클릭하거나 esc 키를 눌렀을 때 Modal, DropDown, SideMenu, HamburgerMenu를 닫는 훅
+ *
+ * @hook
+ * @param {boolean} isOpenElement Modal, DropDown, SideMenu, HamburgerMenu의 열림 유무 상태
+ * @param {Function} setIsOpenElement 같이 Modal, DropDown, SideMenu, HamburgerMenu의 열림 상태를 관리하는 함수
+ * @param {React.RefObject} elementRef Modal, DropDown, SideMenu, HamburgerMenu 영역
+ */
+
+export default function useClose(
+  isOpenElement: boolean,
+  handleClose: () => void,
+  elementRef: React.RefObject<HTMLDivElement>
+) {
+  useEffect(() => {
+    const handleOutsideClick = (e: MouseEvent) => {
+      if (
+        elementRef.current &&
+        !elementRef.current.contains(e.target as Node)
+      ) {
+        handleClose();
+      }
+    };
+    const handleEscapeKey = (e: KeyboardEvent) => {
+      if (e.key === "Escape") {
+        handleClose();
+      }
+    };
+
+    if (isOpenElement) {
+      document.addEventListener("mousedown", handleOutsideClick);
+      document.addEventListener("keydown", handleEscapeKey);
+    }
+
+    return () => {
+      document.removeEventListener("mousedown", handleOutsideClick);
+      document.removeEventListener("keydown", handleEscapeKey);
+    };
+  }, [isOpenElement, handleClose, elementRef]);
+}
--- src/shared/ui/Menu/SideMenu/SideMenu.tsx
+++ src/shared/ui/Menu/SideMenu/SideMenu.tsx
@@ -8,7 +8,7 @@ import {
 } from "@/shared/ui/Menu/SideMenu/type/categoryType";
 import SideMenuTab from "@/shared/ui/Menu/SideMenu/SideMenuTab";
 import useSideMenuStore from "@/stores/useSideMenuStore";
-import useCloseSideMenu from "@/shared/ui/Menu/SideMenu/hooks/useCloseSideMenu";
+import useClose from "@/shared/@common/hooks/useClose";
 
 /**
  * 메인 페이지의 layout.tsx에서 API 요청해서 카테고리 데이터 받아오고 layout.tsx에서 사용하면됨
@@ -27,10 +27,10 @@ const SideMenu = ({ categories }: SideMenuProps) => {
   // SideMenu 영역 지정
   const sideMenuRef = useRef<HTMLDivElement>(null);
   // SideMenu 닫힘 버튼 구현 대신 외부영역 및 ESC로 닫는 훅 호출
-  useCloseSideMenu(isOpenSideMenu, setIsOpenSideMenu, sideMenuRef);
+  useClose(isOpenSideMenu, setIsOpenSideMenu, sideMenuRef);
 
   const commonClass = "py-[25px] px-[10px] h-full bg-black-1C";
-  const openClass = `${commonClass} mobile:relative mobile:z-10 mobile:w-[180px] md:w-[180px] lg:w-[220px]`;
+  const openClass = `${commonClass} mobile:absolute mobile:z-10 mobile:w-[180px] md:w-[180px] lg:w-[220px]`;
   const closeClass = `${commonClass} hidden md:block lg:block md:w-[180px] lg:w-[220px]`;
 
   return (

dbb0b90f40d2b61053715ac5b07aecb90fe8a167
hyunjun9788 <namhyunjun9788@gmail.com>
2024-05-24 20:30:29 +0900

    #25 fix: Icon props 변경

--- src/shared/ui/Icon/Icon.tsx
+++ src/shared/ui/Icon/Icon.tsx
@@ -1,5 +1,5 @@
 import StarIcon from "../../../../public/icon/star.svg";
-import BubbleIcon from "../../../../public/icon/bubble.svg";
+import ReviewIcon from "../../../../public/icon/review.svg";
 import AddIcon from "../../../../public/icon/add.svg";
 import CategoryIcon from "../../../../public/icon/category.svg";
 import CloseIcon from "../../../../public/icon/close.svg";
@@ -8,7 +8,7 @@ import DropUpIcon from "../../../../public/icon/drop-up.svg";
 import GoogleIcon from "../../../../public/icon/google.svg";
 import KakaoIcon from "../../../../public/icon/kakao.svg";
 import KakaoTalkIcon from "../../../../public/icon/kakaotalk.svg";
-import MyPropfileIcon from "../../../../public/icon/my-profile.svg";
+import MyProfileIcon from "../../../../public/icon/my-profile.svg";
 import PhotoIcon from "../../../../public/icon/photo.svg";
 import SaveIcon from "../../../../public/icon/save.svg";
 import SearchIcon from "../../../../public/icon/search.svg";
@@ -19,36 +19,14 @@ import UpNoColorIcon from "../../../../public/icon/up-no-color.svg";
 import UpIcon from "../../../../public/icon/up.svg";
 import VisibilityIcon from "../../../../public/icon/visibility.svg";
 import VisibilityOffIcon from "../../../../public/icon/visibility-off.svg";
-
-type IconType =
-  | "StarIcon"
-  | "BubbleIcon"
-  | "AddIcon"
-  | "CategoryIcon"
-  | "CloseIcon"
-  | "DropDownIcon"
-  | "DropUpIcon"
-  | "GoogleIcon"
-  | "KakaoIcon"
-  | "KakaoTalkIcon"
-  | "MenuIcon"
-  | "MyPropfileIcon"
-  | "PhotoIcon"
-  | "SaveIcon"
-  | "SearchIcon"
-  | "ShareIcon"
-  | "UnSaveIcon"
-  | "UpNoColorIcon"
-  | "UpIcon"
-  | "VisibilityIcon"
-  | "VisibilityOffIcon";
+import { IconProps, IconType } from "@/shared/types/iconType";
 
 const iconTypes: Record<
   IconType,
   React.ComponentType<React.SVGProps<SVGSVGElement>>
 > = {
   StarIcon,
-  BubbleIcon,
+  ReviewIcon,
   AddIcon,
   CategoryIcon,
   CloseIcon,
@@ -58,7 +36,7 @@ const iconTypes: Record<
   KakaoIcon,
   KakaoTalkIcon,
   MenuIcon,
-  MyPropfileIcon,
+  MyProfileIcon,
   PhotoIcon,
   SaveIcon,
   SearchIcon,
@@ -70,17 +48,10 @@ const iconTypes: Record<
   VisibilityOffIcon,
 };
 
-interface IconProps {
-  name: IconType;
-  iconSizeClass: string;
-  color?: string;
-  onClick?: () => void;
-}
-const IconComponent = ({ name, iconSizeClass, color, onClick }: IconProps) => {
+const IconComponent = ({ name, className, onClick }: IconProps) => {
   const Icon = iconTypes[name];
-  return (
-    <Icon className={`${iconSizeClass}`} color={color} onClick={onClick} />
-  );
+
+  return <Icon className={className} onClick={onClick} />;
 };
 
 export default IconComponent;

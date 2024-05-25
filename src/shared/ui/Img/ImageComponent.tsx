1af73e8dedbba23708a636645755a03990fdc577
hyunjun9788 <namhyunjun9788@gmail.com>
2024-05-25 22:38:18 +0900

    #39 refactor: Image 컴포넌트 스타일 리팩토링

--- src/shared/ui/Img/ImageComponent.tsx
+++ src/shared/ui/Img/ImageComponent.tsx
@@ -6,21 +6,15 @@ const typeClasses: Record<ImageProps["type"], string> = {
   product:
     "mobile:w-[140px] mobile:h-[98px] md:w-[227px] md:h-[160px] lg:w-[284px] lg:h-[200px]",
   profile:
-    "mobile:w-[36px] mobile:h-[36px] md:w-[36px] md:h-[36px] lg:w-[42px] lg:h-[42px]",
+    "mobile:w-[36px] mobile:h-[36px] md:w-[36px] md:h-[36px] lg:w-[42px] lg:h-[42px] rounded-full overflow-hidden",
   review:
     "mobile:w-[60px] mobile:h-[60px] md:w-[80px] md:h-[80px] lg:w-[100px] lg:h-[100px]",
 };
 
 const ImageComponent = ({ type, className, src, alt }: ImageProps) => {
-  const isProfile = type === "profile";
   return (
     <div className={twMerge("relative", typeClasses[type], className)}>
-      <Image
-        src={src}
-        fill
-        alt={alt}
-        style={isProfile ? { borderRadius: "50%" } : {}}
-      />
+      <Image src={src} fill alt={alt} sizes="auto" priority={true} />
     </div>
   );
 };

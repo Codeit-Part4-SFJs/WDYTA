a9c45dcabda8188355ec0c8df808d93aa496968e
yeona <aya813@naver.com>
2024-05-25 21:26:26 +0900

    #32 feat: SocialLogin 컴포넌트 생성

---
+++ src/components/Login/SocialLogin.tsx
@@ -0,0 +1,31 @@
+import { IconType } from "@/shared/types/iconType";
+import Icon from "@/shared/ui/Icon/Icon";
+
+const SNS_MAP: { name: IconType }[] = [
+  { name: "GoogleIcon" },
+  { name: "KakaoIcon" },
+];
+
+const SocialLogin = () => {
+  return (
+    <div className="flex flex-col gap-5">
+      <span className=" text-sm lg:text-base font-normal text-gray-6E">
+        SNS로 바로 시작하기
+      </span>
+      <div className="flex gap-5">
+        {SNS_MAP.map((item, index) => (
+          <div
+            key={index}
+            className="w-14 h-14 p-[14px] rounded-full border border-solid border-gray-6E flex items-center justify-center"
+          >
+            <Icon name={item.name} className="w-full h-full text-gray-6E" />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default SocialLogin;
+
+//@TODO-Icon 클릭 시 간편로그인 기능 구현

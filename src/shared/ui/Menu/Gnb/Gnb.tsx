4fc7226b0c77d3e2dab26ab7cd483047ebd2311f
Sumin <csmxx99@naver.com>
2024-05-26 04:09:01 +0900

    [#10] feat: Gnb 유저 기능 추가

--- src/shared/ui/Menu/Gnb/Gnb.tsx
+++ src/shared/ui/Menu/Gnb/Gnb.tsx
@@ -1,12 +1,15 @@
 import GnbLogo from "./GnbLogo";
 import GnbSearch from "./GnbSearch";
+import GnbUser from "./GnbUser";
 
 const Gnb = () => {
   return (
     <header>
       {/* 로고, 서치, 유저기능, 햄버거 메뉴 */}
+      {/* <GnbHamburgerMenu /> */}
       <GnbLogo />
       <GnbSearch />
+      <GnbUser />
     </header>
   );
 };

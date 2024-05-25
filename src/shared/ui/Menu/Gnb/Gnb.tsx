eedac4ab33a42972da693a278f567b9e16a3f3b8
Sumin <csmxx99@naver.com>
2024-05-26 02:30:19 +0900

    [#10] feat: GnbLogo 추가

--- src/shared/ui/Menu/Gnb/Gnb.tsx
+++ src/shared/ui/Menu/Gnb/Gnb.tsx
@@ -1,9 +1,11 @@
+import GnbLogo from "./GnbLogo";
 import GnbSearch from "./GnbSearch";
 
 const Gnb = () => {
   return (
     <header>
       {/* 로고, 서치, 유저기능, 햄버거 메뉴 */}
+      <GnbLogo />
       <GnbSearch />
     </header>
   );

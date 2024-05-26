bd865268b157d77384921d54ffccba2d7696bd22
Sumin <csmxx99@naver.com>
2024-05-26 10:38:51 +0900

    chore: 오타 수정

--- src/stores/storeType.ts
+++ src/stores/storeType.ts
@@ -1,7 +1,7 @@
 // 먼저 전역 변수로 사용하고 싶은 상태와 액션 함수의 타입을 정의해줍니다.
-export interface ExempleState {
-  exemple: string;
-  setExemple: (newExemple: string) => void;
+export interface ExampleState {
+  example: string;
+  setExample: (newExample: string) => void;
 }
 
 export interface SideMenuState {

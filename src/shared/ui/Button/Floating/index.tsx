88638b66800c11541b45b3a56273454cf8f550ff
DESKTOP-142GQLG\82106 <mangang0713@gmail.com>
2024-05-26 16:08:40 +0900

    #23 feat: Floating 버튼 클릭시 alert 생성

--- src/shared/ui/Button/Floating/index.tsx
+++ src/shared/ui/Button/Floating/index.tsx
@@ -5,10 +5,15 @@ interface FloatingProps {
 }
 
 const Floating = ({ location }: FloatingProps) => {
+  const makingAlertNow = () => {
+    alert("Floating이 클릭되었습니다!");
+  };
+
   return (
     <IconComponent
       name="AddIcon"
       className={`w-[60px] fixed main-gradation ${location}`}
+      onClick={makingAlertNow}
     />
   );
 };

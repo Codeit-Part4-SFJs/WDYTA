9260657a372047f3cef604bfc0eb1c6825147731
linason-esc <linason7889@gmail.com>
2024-05-26 17:21:02 +0900

    [#19] feat: ThumbsChip storybook 작성

---
+++ src/shared/ui/Chip/ThumbsChip/ThumbsChip.stories.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { StoryFn, Meta } from "@storybook/react";
+import ThumbsChip from ".";
+import "@/styles/globals.css";
+
+export default {
+  title: "Chip/ThumbsChip",
+  component: ThumbsChip,
+  argTypes: {
+    reviewID: {
+      control: "number",
+    },
+    isLike: {
+      control: "boolean",
+    },
+    likeCount: {
+      control: "number",
+    },
+  },
+} as Meta<typeof ThumbsChip>;
+
+const Template: StoryFn<typeof ThumbsChip> = (args) => <ThumbsChip {...args} />;
+export const exampleThumbsChip = Template.bind({});
+exampleThumbsChip.args = {
+  reviewID: 1,
+  isLike: false,
+  likeCount: 99,
+};
--- src/shared/ui/Chip/ThumbsChip/index.tsx
+++ src/shared/ui/Chip/ThumbsChip/index.tsx
@@ -1,32 +1,55 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Icon from "@/shared/ui/Icon/Icon";
 
 interface ThumbsChipProps {
   reviewID?: number;
   isLike: boolean;
   likeCount: number;
+  onClick?: () => void;
 }
 
-const ThumbsChip = ({ reviewID, isLike, likeCount }: ThumbsChipProps) => {
+const ThumbsChip = ({
+  reviewID,
+  isLike,
+  likeCount,
+  onClick,
+}: ThumbsChipProps) => {
   const [isLiked, setIsLiked] = useState(isLike);
-  const [likedCount, setLikedCount] = useState(likeCount);
+  const [likedCount, setLikedCount] = useState<number | string>(likeCount);
+
+  useEffect(() => {
+    setIsLiked(isLike);
+    setLikedCount(likeCount);
+  }, [isLike, likeCount]);
 
   const likeIconImage = isLiked ? "UpIcon" : "UpNoColorIcon";
   const likeClassName = isLiked ? "text-pink" : "text-gray-9F";
   //좋아요가 세자리 이상이면 text-[12px]로 변경
   let likeCountClassName = "text-[14px]";
-  if (likedCount >= 100) {
+  const count = Number(likedCount);
+  if (count >= 100) {
     likeCountClassName = "text-[12px]";
   }
 
-  //API 연동 시 변경해야함 (좋아요 누르면 post 요청하여 좋아요 수 증가, 좋아요 취소하면 delete 요청하여 좋아요 수 감소)
+  //좋아요가 1000 이상이면 1K+로 변경
+  if (count >= 1000) {
+    setLikedCount("1K+");
+    likeCountClassName = "text-[12px]";
+  }
+  if (count >= 2000) {
+    setLikedCount("2K+");
+    likeCountClassName = "text-[12px]";
+  }
+
+  //임의로 만들어둔 함수입니다!
+  //API 연동 시 props로 함수 내려줘서 변경해야해요! (좋아요 누르면 post 요청하여 좋아요 수 증가, 좋아요 취소하면 delete 요청하여 좋아요 수 감소)
   const handleLike = () => {
     if (isLiked) {
-      setLikedCount(likedCount - 1);
+      setLikedCount(count - 1);
     } else {
-      setLikedCount(likedCount + 1);
+      setLikedCount(count + 1);
     }
     setIsLiked(!isLiked);
   };

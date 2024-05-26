dc193d7af19cfafa8a8e4c6f2f4cabc6e5b9515d
hyunjun9788 <namhyunjun9788@gmail.com>
2024-05-26 15:11:23 +0900

    #37 feat: Loading 컴포넌트 스토리북 구현

--- src/shared/ui/Icon/Loading.tsx
+++ src/shared/ui/Icon/Loading.tsx
@@ -1,12 +1,27 @@
-import { PropsWithChildren } from "react";
+import { twMerge } from "tailwind-merge";
+import { HTMLAttributes, PropsWithChildren } from "react";
 import LoadingIcon from "../../../../public/icon/loading.svg";
+import { LoadingProps } from "./type/iconType";
 
-const Loading = ({ children }: PropsWithChildren) => {
+const Loading = ({
+  iconClassName,
+  textClassName,
+  children,
+}: PropsWithChildren<LoadingProps>) => {
   return (
     <div className="flex flex-col items-center gap-3">
-      <LoadingIcon className="mobile:w-[39.2px] mobile:h-[32px] md:w-[39.2px] md:h-[32px] lg:w-[49px] lg:h-[40px] fill-gray-6E " />
-
-      <p className="mobile:text-lg md:text-lg lg:text-xl text-gray-6E">
+      <LoadingIcon
+        className={twMerge(
+          "mobile:w-[39.2px] mobile:h-[32px] w-[39.2px] h-[32px] lg:w-[49px] lg:h-[40px] fill-gray-6E",
+          iconClassName
+        )}
+      />
+      <p
+        className={twMerge(
+          "mobile:text-lg md:text-lg lg:text-xl text-gray-6E ",
+          textClassName
+        )}
+      >
         {children}
       </p>
     </div>

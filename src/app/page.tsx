9e300cd555123cea6178876e5ab3a265d1dbb124
linason-esc <linason7889@gmail.com>
2024-05-26 17:22:16 +0900

    feat: 홈페이지 초기화

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,30 +1,3 @@
-import CategoryChip from "@/shared/ui/Chip/CategoryChip";
-import CategoryFilterChip from "@/shared/ui/Chip/CategoryFilterChip";
-import CompareChip, { CompareColor } from "@/shared/ui/Chip/CompareChip";
-import RankingChip from "@/shared/ui/Chip/RankingChip";
-import ThumbsChip from "@/shared/ui/Chip/ThumbsChip";
-import { RankingColor } from "@/shared/ui/Chip/RankingChip";
-
 export default function Home() {
-  return (
-    <div>
-      <RankingChip ranking={1} color={RankingColor.PINK} />
-      <RankingChip ranking={2} color={RankingColor.GREEN} />
-      <RankingChip ranking={3} color={RankingColor.GRAY} />
-      {[...Array(11)].map((_, index) => (
-        <CategoryChip key={index} categoryID={index + 1} />
-      ))}
-      <CategoryFilterChip categoryID={0} />
-      {[...Array(11)].map((_, index) => (
-        <CategoryFilterChip key={index} categoryID={index + 1} />
-      ))}
-      <ThumbsChip reviewID={1} isLike={false} likeCount={99} />
-      <CompareChip productName="IPhone 14 Pro Max" color={CompareColor.GREEN} />
-      <CompareChip productName="Galaxy Z Fold 4" color={CompareColor.PINK} />
-    </div>
-  );
+  return <div>홈페이지입니다~</div>;
 }
-
-// export default function Home() {
-//   return <div>홈페이지입니다~</div>;
-// }

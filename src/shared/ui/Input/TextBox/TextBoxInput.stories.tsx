302604407e9ca9355182fd3a141401bf4935a598
yeona <aya813@naver.com>
2024-05-28 00:32:38 +0900

    #32 fix: import 오류 수정

--- src/shared/ui/Input/TextBox/TextBoxInput.stories.tsx
+++ src/shared/ui/Input/TextBox/TextBoxInput.stories.tsx
@@ -1,7 +1,7 @@
 import { FormValues } from '@/shared/types/input';
 import { Meta, StoryFn } from '@storybook/react';
 import { useForm } from 'react-hook-form';
-import TextBoxInput from '.';
+import TextBoxInput from './TextBoxInput';
 
 export default {
   title: 'Input/TextBoxInput',

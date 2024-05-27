4d416ab106deada37a55b19d4542e21f0f458bb6
yeona <aya813@naver.com>
2024-05-27 18:45:03 +0900

    #32 feat: 글자 css 스타일 통일

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -7,8 +7,8 @@ const Login = () => {
     <div className="flex flex-col gap-5 items-center mt-[108px] md:mt-[250px] lg:mt-[181px]">
       <LoginForm />
       <div className="flex gap-2">
-        <p className="text-base text-gray-6E">회원이 아니신가요?</p>
-        <Link className="text-base text-main-blue" href="/register">
+        <p className="text-sm lg:text-base text-gray-6E">회원이 아니신가요?</p>
+        <Link className="text-sm lg:text-base text-main-blue" href="/register">
           회원가입하기
         </Link>
       </div>

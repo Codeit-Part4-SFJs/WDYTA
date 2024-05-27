d4d12515ebd61a38b334a83eea7fb49f07ab976b
yeona <aya813@naver.com>
2024-05-27 19:07:22 +0900

    #32 feat: 로그인 폼 버튼 비활성화 기능 추가

--- src/components/Login/LoginForm.tsx
+++ src/components/Login/LoginForm.tsx
@@ -3,19 +3,30 @@ import { FormValues } from "@/shared/types/input";
 import Button, { ButtonKind } from "@/shared/ui/Button/Button";
 import EmailInput from "@/shared/ui/Input/Email";
 import PasswordInput from "@/shared/ui/Input/Password";
+import { useEffect, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 
 const LoginForm = () => {
   const {
     register,
+    watch,
     handleSubmit,
-    formState: { errors },
+    formState: { errors, isValid },
   } = useForm<FormValues>({ mode: "onChange" });
+
+  const [isDisabled, setIsDisabled] = useState(true);
 
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     console.log(data);
     //@todo API 요청
   };
+
+  const email = watch("email");
+  const password = watch("password");
+
+  useEffect(() => {
+    setIsDisabled(!isValid || !email || !password);
+  }, [email, password, isValid]);
 
   return (
     <form
@@ -27,7 +38,8 @@ const LoginForm = () => {
       <Button
         type="submit"
         kind={ButtonKind.primary}
-        customSize="mt-[20px] w-[335px] md:w-[440px] lg:w-[640px] h-[50px] md:h-[55px] lg:h-[65px]"
+        customSize={`${isDisabled ? "cursor-not-allowed" : "cursor-pointer"} mt-[20px] w-[335px] md:w-[440px] lg:w-[640px] h-[50px] md:h-[55px] lg:h-[65px]`}
+        disabled={isDisabled}
       >
         로그인
       </Button>

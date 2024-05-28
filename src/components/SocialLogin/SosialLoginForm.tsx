d07b2f912a69609bc9d80f154f4c126335f264a2
yeona <aya813@naver.com>
2024-05-28 12:00:17 +0900

    #34 refactor: ESLint 적용 및 폼 유효성 검사 수정

--- src/components/SocialLogin/SosialLoginForm.tsx
+++ src/components/SocialLogin/SosialLoginForm.tsx
@@ -1,40 +1,32 @@
-"use client";
+'use client';
 
-import { FormValues } from "@/shared/types/input";
-import Button, { ButtonKind } from "@/shared/ui/Button/Button";
-import NicknameInput from "@/shared/ui/Input/Nickname";
-import { useEffect, useState } from "react";
-import { SubmitHandler, useForm } from "react-hook-form";
+import { FormValues } from '@/shared/types/input';
+import Button, { ButtonKind } from '@/shared/ui/Button/Button';
+import { NicknameInput } from '@/shared/ui/Input/Nickname';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
 const SocialLoginForm = () => {
   const {
     register,
     handleSubmit,
-    watch,
     formState: { errors, isValid },
-  } = useForm<FormValues>({ mode: "onChange" });
-
-  const [isDisabled, setIsDisabled] = useState(true);
+  } = useForm<FormValues>({ mode: 'onChange' });
 
   const onSubmit: SubmitHandler<FormValues> = (data) => {
     console.log(data);
-    //@todo API 요청
   };
 
-  const nickname = watch("nickname");
-
-  useEffect(() => {
-    setIsDisabled(!isValid || !nickname);
-  }, [isValid, nickname]);
-
   return (
-    <form className="flex flex-col gap-[60px]">
+    <form
+      className="flex flex-col gap-[60px]"
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <NicknameInput register={register} errors={errors} />
       <Button
         type="submit"
         kind={ButtonKind.primary}
-        customSize={`${isDisabled ? "cursor-not-allowed" : "cursor-pointer"} mt-[20px] w-[335px] md:w-[440px] lg:w-[640px] h-[50px] md:h-[55px] lg:h-[65px]`}
-        disabled={isDisabled}
+        customSize={`${!isValid ? 'cursor-not-allowed' : 'cursor-pointer'} mt-[20px] w-[335px] md:w-[440px] lg:w-[640px] h-[50px] md:h-[55px] lg:h-[65px]`}
+        disabled={!isValid}
       >
         가입하기
       </Button>

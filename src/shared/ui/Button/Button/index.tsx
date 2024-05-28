aca53bf06656a4b4198e5a4d100af0099db71bfb
cgw-notebook <mangang0713@gmail.com>
2024-05-28 09:50:44 +0900

    #3 refactor: lint에 맞게 수정

--- src/shared/ui/Button/Button/index.tsx
+++ src/shared/ui/Button/Button/index.tsx
@@ -1,9 +1,9 @@
-import { PropsWithChildren, ButtonHTMLAttributes } from "react";
+import { PropsWithChildren, ButtonHTMLAttributes } from 'react';
 
 export enum ButtonKind {
-  primary = "primary",
-  secondary = "secondary",
-  tertiary = "tertiary",
+  primary = 'primary',
+  secondary = 'secondary',
+  tertiary = 'tertiary',
 }
 export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   kind: ButtonKind;
@@ -11,21 +11,21 @@ export interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
 }
 
 const buttonBase =
-  "flex justify-center items-center gap-[10px] flex-shrink-0 rounded-lg whitespace-nowrap";
+  'flex justify-center items-center gap-[10px] flex-shrink-0 rounded-lg whitespace-nowrap';
 const ButtonStyleByKind = {
   [ButtonKind.primary]: {
-    button: "bg-main-gradation disabled:bg-none disabled:bg-gray-35",
-    p: "text-white group-disabled:text-gray-6E",
+    button: 'bg-main-gradation disabled:bg-none disabled:bg-gray-35',
+    p: 'text-white group-disabled:text-gray-6E',
   },
   [ButtonKind.secondary]: {
     button:
-      "border border-solid border-main-blue border-transparent disabled:border-gray-35 disabled:text-gray-6E",
-    p: "group-disabled:text-gray-6E text-transparent bg-clip-text bg-main-gradation",
+      'border border-solid border-main-blue border-transparent disabled:border-gray-35 disabled:text-gray-6E',
+    p: 'group-disabled:text-gray-6E text-transparent bg-clip-text bg-main-gradation',
   },
   [ButtonKind.tertiary]: {
     button:
-      "bg-transparent border border-solid border-gray-9F disabled:border-gray-35",
-    p: "text-gray-9F group-disabled:text-gray-6E",
+      'bg-transparent border border-solid border-gray-9F disabled:border-gray-35',
+    p: 'text-gray-9F group-disabled:text-gray-6E',
   },
 };
 
@@ -39,6 +39,7 @@ const Button = ({
 }: PropsWithChildren<ButtonProps>) => {
   return (
     <button
+      type="button"
       className={`${customSize} ${buttonBase} ${ButtonStyleByKind[kind].button} group `}
       disabled={disabled}
       {...props}

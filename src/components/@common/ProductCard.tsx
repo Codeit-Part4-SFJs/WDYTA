6d6e3453d0d3c1795573a6c29c91e5ef97fa23b2
hyunjun9788 <namhyunjun9788@gmail.com>
2024-05-30 01:11:41 +0900

    #66 refactor: 제품 카드 공통 컴포넌트 리팩토링 및 임시 데이터 활용

---
+++ src/components/@common/ProductCard.tsx
@@ -0,0 +1,55 @@
+import { Icon } from '@/shared/ui/Icon';
+import { ImageComponent } from '@/shared/ui/Img';
+import Link from 'next/link';
+
+// 추후 이동
+interface ProductTypes {
+  updatedAt: string;
+  createdAt: string;
+  writerId: number;
+  categoryId: number;
+  favoriteCount: number;
+  reviewCount: number;
+  rating: number;
+  image: string;
+  name: string;
+  id: number;
+}
+
+interface ProductCardProps {
+  product: ProductTypes;
+}
+
+const ProductCard = ({ product }: ProductCardProps) => {
+  const { id, image, name, reviewCount, rating, favoriteCount } = product;
+  return (
+    <Link href={`/detail/${id}`}>
+      <div className="flex flex-col items-center p-[10px] lg:pb-[25px] md:pb-[20px] mobile:w-[160px] md:w-[247px] lg:w-[300px] mobile:gap-[10px] md:gap-[20px] gap-[25px] rounded-lg bg-gray-35  ">
+        <ImageComponent type="product" src={image} alt="이미지" />
+
+        <div className="w-full flex flex-col lg:gap-[10px] md:gap-[10px] mobile:gap-[5px] lg:px-[10px] md:px-[6.5px] ">
+          <div className="text-[14px] md:text-[16px] lg:text-[18px] text-gray-F1 truncate">
+            {name}
+          </div>
+
+          <div className="flex justify-between mobile:flex-col mobile:gap-[5px] mobile:text-[12px] md:text-[14px] lg:text-[16px] text-gray-6E">
+            <div className="flex items-center gap-[15px]">
+              <p>리뷰 {reviewCount}</p>
+              <p>찜 {favoriteCount}</p>
+            </div>
+
+            <div className="flex items-center gap-[2px]">
+              <Icon
+                name="StarIcon"
+                className="mobile:w-[12px] mobile:h-[12px] md:w-[15px] md:h-[15px] lg:w-[16px] lg:h-[16px] fill-yellow"
+              />
+              <p className="text-gray-9F">{rating}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default ProductCard;

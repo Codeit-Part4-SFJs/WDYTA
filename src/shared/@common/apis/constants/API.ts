bd8b233ffeffd588fbd8ae49a31121296dc89457
yeona <aya813@naver.com>
2024-05-30 16:20:04 +0900

    common: 공통 API 및 auth 파일 삭제

---
+++ src/shared/@common/apis/constants/API.ts
@@ -0,0 +1,10 @@
+export const API = Object.freeze({
+  USER: '/users',
+  REVIEW: '/reviews',
+  PRODUCT: '/products',
+  OAUTH: '/oauthApps',
+  IMAGE: '/images',
+  FOLLOW: '/follow',
+  CATEGORY: '/categories',
+  AUTH: '/auth',
+} as const);

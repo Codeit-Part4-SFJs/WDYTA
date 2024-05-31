8ca4c533179ce32c4a72e31e17a9dfb18a4554a8
hyunjun9788 <namhyunjun9788@gmail.com>
2024-05-31 21:19:13 +0900

    #68 feat: profileCard pc 버전 ui 구현

--- src/components/Profile/ProfileCard.tsx
+++ src/components/Profile/ProfileCard.tsx
@@ -6,7 +6,7 @@ import { useEffect, useState } from 'react';
 
 const profileData = {
   description:
-    '안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요',
+    '안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요안녕하세요',
   image:
     'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Mogazoa/user/158/1716804166536/KakaoTalk_20240527_185342395.jpg',
   nickname: '다살거야',
@@ -26,16 +26,18 @@ const ProfileCard = () => {
     }
   }, []);
   return (
-    <div className="flex flex-col items-center justify-around  bg-gray-25 w-[335px] h-[466px] lg:w-[340px] lg:h-[603px] md:w-[509px] md:h-[451] rounded-xl border border-solid border-gray-35">
+    <div className="flex flex-col items-center justify-center gap-[30px]  bg-gray-25 w-[335px] h-[466px] lg:w-[340px] lg:h-[603px] md:w-[509px] md:h-[451] rounded-xl border border-solid border-gray-35">
       <ImageComponent
         type="profile"
         src={profileData.image}
         className="lg:w-[180px] lg:h-[180px]"
         alt="프로필 이미지"
       />
-      <div className=" flex flex-col items-center lg:gap-[20px]">
+      <div className=" flex flex-col items-center lg:gap-[20px] w-[300px] min-h-[66px] text-center">
         <p className="text-[24px] text-gray-F1">{profileData?.nickname}</p>
-        <p className="text-gray-6E truncate">{profileData?.description}</p>
+        <p className="text-gray-6E w-[300px] h-[70px] line-clamp-3">
+          {profileData?.description}
+        </p>
       </div>
       <div className="flex justify-between lg:w-[184px] lg:h-[53px]">
         <div className="flex flex-col items-center">

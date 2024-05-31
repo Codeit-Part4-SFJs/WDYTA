d8f81d965a3a14800166a59b944eb2f3a2a1306f
hyunjun9788 <namhyunjun9788@gmail.com>
2024-05-31 16:32:13 +0900

    #75 feat: product, follow api 세팅

---
+++ src/shared/@common/apis/product.ts
@@ -0,0 +1,104 @@
+import { API_PRODUCT } from './constants/API';
+
+interface ProductProps {
+  categoryId: number;
+  image: string;
+  description: string;
+  name: string;
+}
+/**
+ * 상품목록조회
+ */
+export const getProductList = () => {
+  return fetch(API_PRODUCT.PRODUCT, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+/**
+ * 상품 생성
+ * @param data: categoryId(카테고리 Id), image(상품 이미지), description(상품 설명), name(상품 이름)
+ */
+export const postCreateProduct = (data: ProductProps) => {
+  return fetch(API_PRODUCT.PRODUCT, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+};
+
+/**
+ * 상세 상품 조회
+ * @param productId: 상품 Id
+ */
+export const getDetailProduct = (productId: number) => {
+  return fetch(API_PRODUCT.BY_ID(productId), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+/**
+ * 상품 수정
+ * @param productId : 상품 Id
+ * @param data: categoryId(카테고리 Id), image(상품 이미지), description(상품 설명), name(상품 이름)
+
+ */
+export const patchProduct = (productId: number, data: ProductProps) => {
+  return fetch(API_PRODUCT.BY_ID(productId), {
+    method: 'PATCH',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
+};
+
+/**
+ * 상품 리뷰 목록 조회
+ * @param productId : 상품 Id
+ */
+
+export const getProductReviewList = (productId: number) => {
+  return fetch(API_PRODUCT.REVIEWS(productId), {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+/**
+ * 상품 찜하기
+ * @param productId : 상품 Id
+ */
+
+export const postFavoriteProduct = (productId: number) => {
+  return fetch(API_PRODUCT.FAVORITE(productId), {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+/**
+ * 상품 찜하기 취소
+ * @param productId : 상품 Id
+ */
+
+export const deleteFavoriteProduct = (productId: number) => {
+  return fetch(API_PRODUCT.FAVORITE(productId), {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};

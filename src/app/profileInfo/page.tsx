af52c7e1e22e14b5b8ec2912bf8860fe67e337eb
hyunjun9788 <namhyunjun9788@gmail.com>
2024-06-01 02:34:32 +0900

    #68 feat: 프로필 페이지 반응형 구현

--- src/app/profileInfo/page.tsx
+++ src/app/profileInfo/page.tsx
@@ -3,7 +3,7 @@ import ActivityCard from '@/components/Profile/ActivityCard';
 import ProfileCard from '@/components/Profile/ProfileCard';
 // import ProfileCard from '@/components/Profile/ProfileCard';
 
-const data = {
+const data1 = {
   updatedAt: '2024-05-29T15:11:37.143Z',
   createdAt: '2024-05-29T15:11:37.143Z',
   writerId: 1,
@@ -17,14 +17,28 @@ const data = {
   id: 1,
 };
 
+const data2 = {
+  updatedAt: '2024-05-29T15:11:37.143Z',
+  createdAt: '2024-05-29T15:11:37.143Z',
+  writerId: 1,
+  categoryId: 1,
+  favoriteCount: 34,
+  reviewCount: 129,
+  rating: 4.7,
+  image:
+    'https://sprint-fe-project.s3.ap-northeast-2.amazonaws.com/Mogazoa/user/158/1717172068396/macbookkkk.jpg',
+  name: 'stringxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx',
+  id: 1,
+};
+
 export default function Profile() {
   return (
-    <main className="flex justify-center lg:gap-[70px] py-[50px]">
+    <main className="flex justify-center md:flex-col md:items-center md:min-w-[509px] lg:gap-[70px] p-[52px]">
       <ProfileCard />
       <div className="flex flex-col gap-[80px]">
-        <section className="flex flex-col lg:gap-[30px]">
+        <section className="flex flex-col lg:gap-[30px] md:gap-[30px]">
           <h1 className="text-[20px] text-gray-F1">활동 내역</h1>
-          <div className="flex justify-between">
+          <div className="flex justify-between lg:gap-[20px]">
             <ActivityCard title="남긴 별점 평균" icon="StarIcon" />
             <ActivityCard title="남긴 리뷰" icon="ReviewIcon" />
             <ActivityCard title="남긴 별점 평균" />
@@ -40,14 +54,14 @@ export default function Profile() {
           </ul>
 
           <div className="grid grid-cols-3 gap-[20px]">
-            <ProductCard product={data} />
-            <ProductCard product={data} />
-            <ProductCard product={data} />
-            <ProductCard product={data} />
-            <ProductCard product={data} />
-            <ProductCard product={data} />
-            <ProductCard product={data} />
-            <ProductCard product={data} />
+            <ProductCard product={data1} />
+            <ProductCard product={data2} />
+            <ProductCard product={data1} />
+            <ProductCard product={data2} />
+            <ProductCard product={data1} />
+            <ProductCard product={data2} />
+            <ProductCard product={data1} />
+            <ProductCard product={data2} />
           </div>
         </section>
       </div>

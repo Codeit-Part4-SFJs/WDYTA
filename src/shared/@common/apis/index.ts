7998ef548067aea4d0bff4a7e053e68bf00a7a5a
Sumin <csmxx99@naver.com>
2024-06-02 12:33:54 +0900

    [#77] api: 공용 api 세팅

---
+++ src/shared/@common/apis/index.ts
@@ -0,0 +1,6 @@
+export * from '@/shared/@common/apis/auth';
+export * from '@/shared/@common/apis/category';
+export * from '@/shared/@common/apis/image';
+export * from '@/shared/@common/apis/oauth';
+export * from '@/shared/@common/apis/review';
+export * from '@/shared/@common/apis/user';

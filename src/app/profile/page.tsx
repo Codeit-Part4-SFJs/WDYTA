0ccabcb3ffc2685014de14971c77703b3cb99643
hyunjun9788 <namhyunjun9788@gmail.com>
2024-06-03 01:41:19 +0900

    #68 chore: 폴더명 변경

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -4,17 +4,14 @@ import ActivitySection from '@/components/Profile/ActivitySection';
 import ProductSection from '@/components/Profile/ProductSection';
 import ProfileCard from '@/components/Profile/ProfileCard';
 
-// import ProfileCard from '@/components/Profile/ProfileCard';
-
 export default function Profile() {
   return (
-    <main className="flex justify-center md:flex-col mobile:flex-col  md:items-center mobile:items-center md:min-w-[509px] mobile:min-w-[335px] lg:gap-[70px] gap-[60px] p-[52px]">
+    <main className="flex justify-center items-start md:flex-col mobile:flex-col md:items-center mobile:items-center md:min-w-[509px] mobile:min-w-[335px] lg:gap-[70px] gap-[60px] p-[52px]">
       <ProfileCard />
       <div className="flex flex-col lg:gap-[80px] gap-[60px] mobile:w-[335px]">
         <ActivitySection />
         <ProductSection />
       </div>
-      {/* <ProfileCard /> */}
     </main>
   );
 }

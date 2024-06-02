caca54209446883e76a56552f48a2b5ac7165fa9
Sumin <csmxx99@naver.com>
2024-06-02 12:49:30 +0900

    chore: 빌드 오류 해결을 위한 빈 파일 삭제

--- src/components/Detail/types/index.ts
+++ src/components/Detail/types/index.ts
@@ -1,3 +1,2 @@
 export * from '@/components/Detail/types/ProductStatisticsType';
 export * from '@/components/Detail/types/ProductReviewsType';
-export * from '@/components/Detail/types/ProductDetailType';

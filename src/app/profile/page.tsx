3347f0cf0f3a242e2f3b8e3ea3688dd04d9608c0
hyunjun9788 <namhyunjun9788@gmail.com>
2024-06-04 00:18:57 +0900

    #68 chore: profile 컴포넌트 분리

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,17 +1,5 @@
-import {
-  ActivitySection,
-  ProductSection,
-  ProfileCard,
-} from '@/components/Profile';
+import { ProfilePageComponent } from '@/components/Profile';
 
 export default function Profile() {
-  return (
-    <main className="flex justify-center items-start md:flex-col mobile:flex-col md:items-center mobile:items-center md:min-w-[509px] mobile:min-w-[335px] lg:gap-[70px] gap-[60px] py-[52px] lg:px-[20px] md:px-[100px] mobile:px-[21px] ">
-      <ProfileCard />
-      <div className="flex flex-col lg:gap-[80px] gap-[60px] md:w-full mobile:w-full">
-        <ActivitySection />
-        <ProductSection />
-      </div>
-    </main>
-  );
+  return <ProfilePageComponent />;
 }
--- src/components/Profile/index.ts
+++ src/components/Profile/index.ts
@@ -2,3 +2,4 @@ export * from '@/components/Profile/ActivityCard';
 export * from '@/components/Profile/ActivitySection';
 export * from '@/components/Profile/ProductSection';
 export * from '@/components/Profile/ProfileCard';
+export * from '@/components/Profile/ProfilePageComponent';

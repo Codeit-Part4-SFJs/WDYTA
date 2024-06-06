a291ab847bdb526e22526beb0fd8d9a6a9c539ce
Sumin <csmxx99@naver.com>
2024-06-07 08:49:03 +0900

    chore: 대소문자 변경 이슈 해결

--- src/components/Detail/types/index.ts
+++ src/components/Detail/types/index.ts
@@ -1,3 +1,3 @@
 export * from '@/components/Detail/types/productDetailType';
-export * from '@/components/Detail/types/productReviewsType';
 export * from '@/components/Detail/types/productStatisticsType';
+export * from '@/components/Detail/types/productReviewsType';

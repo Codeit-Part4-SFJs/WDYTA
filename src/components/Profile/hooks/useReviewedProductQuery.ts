54653316169ee99538ae6769046e698ebec1bd91
hyunjun9788 <namhyunjun9788@gmail.com>
2024-06-07 02:22:36 +0900

    #90 fix: useSuspenseQuery로 변경

---
+++ src/components/Profile/hooks/useReviewedProductQuery.ts
@@ -0,0 +1,16 @@
+import { useSuspenseQuery } from '@tanstack/react-query';
+import { getUserReviewedProducts } from '@/shared/@common/apis';
+import { API_USERS } from '@/shared/@common/apis/constants/API';
+
+function useReviewedProductQuery(userId: number) {
+  return useSuspenseQuery({
+    queryKey: [API_USERS.REVIEW, userId],
+    queryFn: async () => {
+      if (!userId) return null;
+      const response = await getUserReviewedProducts(userId);
+      return response.json();
+    },
+  });
+}
+
+export default useReviewedProductQuery;
--- src/components/Profile/hooks/useUserFolloweeQuery.ts
+++ src/components/Profile/hooks/useUserFolloweeQuery.ts
@@ -1,9 +1,9 @@
-import { useQuery } from '@tanstack/react-query';
+import { useSuspenseQuery } from '@tanstack/react-query';
 import { getUserFollowees } from '@/shared/@common/apis';
 import { API_USERS } from '@/shared/@common/apis/constants/API';
 
 function useUserFolloweeQuery(userId: number) {
-  return useQuery({
+  return useSuspenseQuery({
     queryKey: [API_USERS, userId],
     queryFn: async () => {
       if (!userId) return null;

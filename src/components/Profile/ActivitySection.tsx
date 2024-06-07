af94ebcc41f956a4e759894a8b317a31e4fb2b18
hyunjun9788 <namhyunjun9788@gmail.com>
2024-06-07 20:57:01 +0900

    #90 fix: 프로필 페이지 드롭다운 변경

--- src/components/Profile/ActivitySection.tsx
+++ src/components/Profile/ActivitySection.tsx
@@ -10,7 +10,7 @@ const activityData: ActivityCardProps[] = [
 export const ActivitySection = () => {
   return (
     <section className="flex flex-col gap-[30px]">
-      <h1 className="lg:text-[20px] text-[18px]  text-gray-F1">활동 내역</h1>
+      <h1 className="lg:text-xl text-lg  text-gray-F1">활동 내역</h1>
       <div className="flex justify-between lg:gap-[20px] gap-[15px]">
         {activityData.map((activity) => (
           <ActivityCard

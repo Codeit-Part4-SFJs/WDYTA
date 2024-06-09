d75812e75b3ba42199d73a2f48f01aef5d6c4afd
hyunjun9788 <namhyunjun9788@gmail.com>
2024-06-10 03:18:55 +0900

    #90 profile 카드 useParams 로직 변경

--- src/components/Profile/ProfileCard.tsx
+++ src/components/Profile/ProfileCard.tsx
@@ -8,23 +8,34 @@ import { useEffect } from 'react';
 import useUserInfoSuspenseQuery from '@/components/Profile/hooks/useUserInfoSuspenseQuery';
 import useFollowMutation from '@/components/Profile/hooks/useFollowMutation';
 import useUnFollowMutation from '@/components/Profile/hooks/useUnFollowMutation';
-import { PROFILE_DEFAULT_IMAGE } from '@/components/Profile/constants/PROFILE_DEFAULT_IMAGE';
+import { PROFILE_DEFAULT_IMAGE } from '@/components/Profile/constants/profileDefaultImage';
+import { useParams, useRouter } from 'next/navigation';
 
 interface ProfileCardProps {
-  loginedId?: number | null;
+  loginedId: number;
   accessToken: string;
-  currentProfileId: number;
 }
 
-export const ProfileCard = ({
-  loginedId,
-  accessToken,
-  currentProfileId,
-}: ProfileCardProps) => {
+export const ProfileCard = ({ loginedId, accessToken }: ProfileCardProps) => {
+  const router = useRouter();
+  const { userId } = useParams();
+
+  const currentProfileId = Number(userId) || loginedId;
+
   const { data: userInfoData } = useUserInfoSuspenseQuery(
-    Number(currentProfileId),
+    currentProfileId,
     accessToken,
   );
+
+  const {
+    image,
+    nickname,
+    description,
+    isFollowing,
+    followersCount,
+    followeesCount,
+  } = userInfoData;
+
   const { setUserInfoData } = useUserInfoStore();
 
   useEffect(() => {
@@ -35,13 +46,11 @@ export const ProfileCard = ({
 
   const { mutate: responseFollowMutate } = useFollowMutation();
   const { mutate: responseUnFollowMutate } = useUnFollowMutation();
-  const isFollowing = userInfoData?.isFollowing;
   const isMyProfile = currentProfileId === loginedId;
 
-  const FollowBtnKind = isFollowing ? ButtonKind.tertiary : ButtonKind.primary;
-  const FollowBtnText = isFollowing ? '팔로우 취소' : '팔로우';
+  const followBtnKind = isFollowing ? ButtonKind.tertiary : ButtonKind.primary;
+  const followBtnText = isFollowing ? '팔로우 취소' : '팔로우';
 
-  // console.log(followeeInfo);
   const handleClickFollow = () => {
     responseFollowMutate({ currentProfileId, accessToken });
   };
@@ -50,7 +59,7 @@ export const ProfileCard = ({
   };
   const handleSignOut = async () => {
     await logoutAction();
-    window.location.reload();
+    router.replace('/');
   };
 
   const buttonCustomSize =
@@ -59,22 +68,22 @@ export const ProfileCard = ({
     <section className="flex flex-col items-center justify-center gap-[42px] pt-[40px] pb-[30px] px-[30px] md:gap-[25px] mobile:gap-[35px] lg:min-w-[340px] md:w-full mobile:w-full rounded-xl border border-solid bg-gray-25 border-gray-35">
       <ImageComponent
         type="profile"
-        src={userInfoData?.image || PROFILE_DEFAULT_IMAGE}
+        src={image || PROFILE_DEFAULT_IMAGE}
         className="lg:w-[180px] lg:h-[180px] md:w-[120px] md:h-[120px] mobile:w-[120px] mobile:h-[120px]"
         alt="프로필 이미지"
       />
       <div className="flex flex-col items-center lg:gap-[20px] md:gap-[10px] mobile:gap-[10px] lg:w-[300px] lg:min-h-[66px] md:w-full mobile:w-full text-center">
         <p className="lg:text-[24px] md:text-[20px]  text-gray-F1">
-          {userInfoData?.nickname}
+          {nickname}
         </p>
         <p className="text-gray-6E w-[300px] md:w-full mobile:w-full md:text-[14px] mobile:text-[14px]">
-          {userInfoData?.description}
+          {description}
         </p>
       </div>
       <div className="flex justify-between lg:w-[184px] lg:h-[53px] md:w-[234px] md:h-[48px] mobile:w-[194px] mobile:h-[48px]">
         <div className="flex flex-col items-center">
           <div className="lg:text-[20px] md:text-[18px] text-gray-F1">
-            {userInfoData?.followersCount || 0}
+            {followersCount || 0}
           </div>
           <div className="lg:text-[16px] md:text-[14px] text-gray-9F">
             팔로워
@@ -83,7 +92,7 @@ export const ProfileCard = ({
         <div className="border border-solid border-gray-35" />
         <div className="flex flex-col items-center">
           <div className="lg:text-[20px] md:text-[18px] text-gray-F1">
-            {userInfoData?.followeesCount || 0}
+            {followeesCount || 0}
           </div>
           <div className="lg:text-[16px] md:text-[14px] text-gray-9F">
             팔로잉
@@ -107,10 +116,10 @@ export const ProfileCard = ({
         ) : (
           <Button
             onClick={!isFollowing ? handleClickFollow : handleClickUnFollow}
-            kind={FollowBtnKind}
+            kind={followBtnKind}
             customSize={buttonCustomSize}
           >
-            {FollowBtnText}
+            {followBtnText}
           </Button>
         )}
       </div>
--- src/components/Profile/ProfilePageComponent.tsx
+++ src/components/Profile/ProfilePageComponent.tsx
@@ -3,9 +3,9 @@ import { ProductSection } from '@/components/Profile/ProductSection';
 import { ProfileCard } from '@/components/Profile/ProfileCard';
 
 interface ProfilePageComponentProps {
-  loginedId: string | null;
+  loginedId: number;
   accessToken: string;
-  currentProfileId: string | null;
+  currentProfileId: number;
 }
 
 export const ProfilePageComponent = ({
@@ -15,14 +15,10 @@ export const ProfilePageComponent = ({
 }: ProfilePageComponentProps) => {
   return (
     <main className="flex justify-center items-start md:flex-col mobile:flex-col md:items-center mobile:items-center md:min-w-[509px] mobile:min-w-[335px] lg:gap-[70px] gap-[60px] py-[52px] lg:px-[20px] md:px-[100px] mobile:px-[21px] ">
-      <ProfileCard
-        loginedId={Number(loginedId)}
-        currentProfileId={Number(currentProfileId)}
-        accessToken={accessToken}
-      />
+      <ProfileCard loginedId={loginedId} accessToken={accessToken} />
       <div className="flex flex-col grow lg:gap-[80px] gap-[60px] max-w-[940px] md:w-full mobile:w-full">
         <ActivitySection />
-        <ProductSection currentProfileId={Number(currentProfileId)} />
+        <ProductSection currentProfileId={currentProfileId} />
       </div>
     </main>
   );

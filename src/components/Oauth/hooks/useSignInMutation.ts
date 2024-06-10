c3723103e44952f3f9cd874024566c370696ac0d
yeona <aya813@naver.com>
2024-06-10 22:22:13 +0900

    #98 feat: 소셜로그인, 회원가입 mutation 추가

---
+++ src/components/Oauth/hooks/useSignInMutation.ts
@@ -0,0 +1,28 @@
+import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import loginAction from '@/components/Login/loginAction';
+import { AuthProps, postSimpleSignIn } from '@/shared/@common/apis';
+
+const useSignInMutation = (provider: string) => {
+  const router = useRouter();
+  return useMutation({
+    mutationFn: async (data: AuthProps) => {
+      const response = await postSimpleSignIn(provider, data);
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+      }
+      return response.json();
+    },
+    onSuccess: async (data) => {
+      await loginAction(data);
+      router.push('/');
+    },
+    onError: (error) => {
+      alert(error.message);
+      router.push('/oauth/signup');
+    },
+  });
+};
+
+export default useSignInMutation;
---
+++ src/components/Oauth/hooks/useSignUpMutation.ts
@@ -0,0 +1,27 @@
+import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/navigation';
+import loginAction from '@/components/Login/loginAction';
+import { AuthProps, postSimpleSignUp } from '@/shared/@common/apis';
+
+const useSignUpMutation = (provider: string) => {
+  const router = useRouter();
+  return useMutation({
+    mutationFn: async (data: AuthProps) => {
+      const response = await postSimpleSignUp(provider, data);
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw new Error(errorData.message);
+      }
+      return response.json();
+    },
+    onSuccess: async (data) => {
+      await loginAction(data);
+      router.push('/');
+    },
+    onError: (error) => {
+      alert(error.message);
+    },
+  });
+};
+
+export default useSignUpMutation;

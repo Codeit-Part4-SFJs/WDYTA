b9fa1c7d2aefab9adabd162c12302134d4f689e3
yeona <aya813@naver.com>
2024-06-11 12:25:03 +0900

    #104 feat: 로그인 로딩 화면 구현

--- src/app/oauth/page.tsx
+++ src/app/oauth/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import useSignInMutation from '@/components/Oauth/hooks/useSignInMutation';
+import { Loading } from '@/shared/ui/Icon';
 import { useSearchParams } from 'next/navigation';
 import { useEffect } from 'react';
 
@@ -21,7 +22,13 @@ const Oauth = () => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return null;
+  return (
+    <div className="absolute inset-0 flex justify-center items-center">
+      <Loading textClassName="text-center">
+        로그인 중입니다. <br /> 잠시만 기다려주세요!
+      </Loading>
+    </div>
+  );
 };
 
 export default Oauth;

abb4a2073a0e84bde9f1465be5f1b78e934ed6b7
Sumin <csmxx99@naver.com>
2024-06-13 02:39:58 +0900

    [#113] feat: 공통 모달 컴포넌트 제작

--- src/shared/ui/Modal/Modal.tsx
+++ src/shared/ui/Modal/Modal.tsx
@@ -3,8 +3,15 @@
 import { ReactNode, useEffect, useRef } from 'react';
 import { useCloseModal } from '@/shared/@common/hooks';
 import { useRouter } from 'next/navigation';
+import { Icon } from '@/shared/ui/Icon';
 
-export const Modal = ({ children }: { children: ReactNode }) => {
+export const Modal = ({
+  children,
+  closeIcon,
+}: {
+  children: ReactNode;
+  closeIcon: boolean;
+}) => {
   const router = useRouter();
   const modalRef = useRef<HTMLDivElement>(null);
 
@@ -22,8 +29,21 @@ export const Modal = ({ children }: { children: ReactNode }) => {
   }, []);
 
   return (
-    <div>
-      <div ref={modalRef}>{children}</div>
+    <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-full z-[999] overflow-auto flex justify-center items-center bg-black bg-opacity-60 backdrop-blur">
+      <div
+        ref={modalRef}
+        className="w-auto h-auto mobile:p-[15px] md:p-[20px] lg:p-[20px] flex justify-start items-end flex-col rounded-2xl bg-black-1C"
+      >
+        <button onClick={handleClose} type="button">
+          {closeIcon && (
+            <Icon
+              name="CloseIcon"
+              className="mobile:w-[24px] mobile:h-[24px] md:w-[36px] md:h-[36px] lg:w-[40px] lg:h-[40px] fill-gray-F1"
+            />
+          )}
+        </button>
+        {children}
+      </div>
     </div>
   );
 };

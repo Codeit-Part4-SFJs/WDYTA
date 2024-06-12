a7e5860f029997e0bf42325402ef2fc309e138f2
hyunjun9788 <namhyunjun9788@gmail.com>
2024-06-12 17:55:24 +0900

    #90 chore: 불필요 파일 삭제

--- src/app/profile/[userId]/queryKeyFactories.ts
+++
@@ -1,6 +0,0 @@
-export const ProfileKeys = {
-  all: ['user'] as const,
-  user: (userId: number) => [...ProfileKeys.all, userId] as const,
-  productCard: (userId: number, queryKey: string) =>
-    [...ProfileKeys.user(userId), queryKey] as const,
-};

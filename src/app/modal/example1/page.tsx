73ac77b8ac750a08ed74518fe0cbcc5486a0052d
Sumin <csmxx99@naver.com>
2024-06-13 01:41:27 +0900

    [#113] feat: Parallel Routes 과 Intercepting Routes 사용을 위한 폴더 구조 적립 및 예시 모달 만들기

---
+++ src/app/modal/example1/page.tsx
@@ -0,0 +1,11 @@
+import { Modal } from '@/shared/ui/Modal';
+
+const ExampleModal = () => {
+  return (
+    <Modal>
+      <div>예시 1</div>
+    </Modal>
+  );
+};
+
+export default ExampleModal;
---
+++ src/app/modal/example2/page.tsx
@@ -0,0 +1,11 @@
+import { Modal } from '@/shared/ui/Modal';
+
+const ExampleModal = () => {
+  return (
+    <Modal>
+      <div>예시 2</div>
+    </Modal>
+  );
+};
+
+export default ExampleModal;

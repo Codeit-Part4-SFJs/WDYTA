b287bc408db701df93e4beca8515b1f11e460926
yeona <aya813@naver.com>
2024-06-13 14:17:23 +0900

    #117 fix: action 오류

--- src/app/@modal/(.)modal/example1/page.tsx
+++ src/app/@modal/(.)modal/example1/page.tsx
@@ -6,7 +6,11 @@ import { Modal } from '@/shared/ui/Modal';
 const ExampleModal = () => {
   return (
     <Modal size="medium" closeIcon>
-      <ReviewModal accessToken="asdfasdf" categoryId={4} name="dkssud" />
+      <ReviewModal
+        accessToken="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MTY0LCJ0ZWFtSWQiOiI0LTE5IiwiaWF0IjoxNzE4MjAxMTg5LCJpc3MiOiJzcC1tb2dhem9hIn0.IUV_94s1EqzVlA98DJf0F6QQKeIwxGoQttB2jrFbWeg"
+        categoryId={7}
+        name="Sony WH-1000XM3"
+      />
     </Modal>
   );
 };

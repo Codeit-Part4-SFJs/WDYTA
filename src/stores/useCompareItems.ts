5fe2ab9daeb1512075e2a5ed22c45ada13af59dd
DESKTOP-142GQLG\82106 <mangang0713@gmail.com>
2024-06-13 23:41:13 +0900

    chore: devtools 추가

--- src/stores/useCompareItems.ts
+++ src/stores/useCompareItems.ts
@@ -9,11 +9,16 @@ interface CompareItemsState {
   removeSecondItem: () => void;
 }
 
-export const useCompareItems = create<CompareItemsState>((set) => ({
-  firstItem: 0,
-  changingFirstItem: (id) => set(() => ({ firstItem: id })),
-  removeFirstItem: () => set(() => ({ firstItem: 0 })),
-  secondItem: 0,
-  changingSecondItem: (id) => set(() => ({ secondItem: id })),
-  removeSecondItem: () => set(() => ({ secondItem: 0 })),
-}));
+export const useCompareItems = create<CompareItemsState>()(
+  devtools(
+    (set) => ({
+      firstItem: 0,
+      changingFirstItem: (id) => set(() => ({ firstItem: id })),
+      removeFirstItem: () => set(() => ({ firstItem: 0 })),
+      secondItem: 0,
+      changingSecondItem: (id) => set(() => ({ secondItem: id })),
+      removeSecondItem: () => set(() => ({ secondItem: 0 })),
+    }),
+    { name: 'CompareItemsStore' },
+  ),
+);

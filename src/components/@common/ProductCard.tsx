a6ecbf978d8d72ae0f61a1bda92d8f6e446930a3
hyunjun9788 <namhyunjun9788@gmail.com>
2024-06-13 22:20:53 +0900

    #121 feat: card 최소 너비 추가

--- src/components/@common/ProductCard.tsx
+++ src/components/@common/ProductCard.tsx
@@ -8,10 +8,10 @@ const ProductCard = ({ product }: { product: ProductTypes }) => {
     product;
   return (
     <Link href={`/${categoryId}/${id}`}>
-      <div className="flex flex-col items-center justify-center pt-[10px] lg:pb-[20px] md:pb-[15px] mobile:pb-[10px] px-[10px] mobile:w-full lg:max-w-[300px] mobile:gap-[10px] md:gap-[20px] gap-[25px] rounded-lg border-gray-35 bg-gray-25">
+      <div className="flex flex-col items-center justify-center pt-[10px] lg:pb-[20px] md:pb-[15px] mobile:pb-[10px] px-[10px] mobile:w-full lg:max-w-[300px] lg:min-w-[180px] mobile:gap-[10px] md:gap-[20px] gap-[25px] rounded-lg border-gray-35 bg-gray-25">
         <ImageComponent type="product" src={image} alt="이미지" />
 
-        <div className="w-full flex flex-col gap-[10px] mobile:gap-[5px] lg:px-[20px] md:px-[16.5px] mobile:px-[10px]">
+        <div className="w-full flex flex-col gap-[10px] mobile:gap-[5px] lg:px-[20px] md:px-[16px] mobile:px-[10px]">
           <p className="text-[14px] md:text-[16px] lg:text-[18px] text-gray-F1 truncate">
             {name}
           </p>

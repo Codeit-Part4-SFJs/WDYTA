59aa578ac381e80d107db264aed7a67655a2c7cd
hyunjun9788 <namhyunjun9788@gmail.com>
2024-06-16 22:28:55 +0900

    #66 fix: 상품 카드 컴포넌트 flex-wrap 적용

--- src/components/@common/ProductCard.tsx
+++ src/components/@common/ProductCard.tsx
@@ -2,13 +2,14 @@ import { Icon } from '@/shared/ui/Icon';
 import { ImageComponent } from '@/shared/ui/Img';
 import Link from 'next/link';
 import { ProductTypes } from '@/components/Profile/types/productType';
+import { formatCount } from '@/shared/@common/utils';
 
 const ProductCard = ({ product }: { product: ProductTypes }) => {
   const { categoryId, id, image, name, reviewCount, rating, favoriteCount } =
     product;
   return (
     <Link href={`/${categoryId}/${id}`}>
-      <div className="flex flex-col items-center justify-center pt-[10px] lg:pb-[20px] md:pb-[15px] mobile:pb-[10px] px-[10px] mobile:w-full lg:max-w-[300px] lg:min-w-[180px] mobile:gap-[10px] md:gap-[20px] gap-[25px] rounded-lg border-gray-35 bg-gray-25">
+      <div className="flex flex-col items-center justify-start pt-[10px] lg:pb-[20px] md:pb-[15px] mobile:pb-[10px] px-[10px] h-full mobile:w-full lg:max-w-[300px] mobile:gap-[10px] md:gap-[20px] gap-[25px] rounded-lg border-gray-35 bg-gray-25">
         <ImageComponent type="product" src={image} alt="이미지" />
 
         <div className="w-full flex flex-col gap-[10px] mobile:gap-[5px] lg:px-[20px] md:px-[16px] mobile:px-[10px]">
@@ -16,10 +17,10 @@ const ProductCard = ({ product }: { product: ProductTypes }) => {
             {name}
           </p>
 
-          <div className="flex justify-between mobile:flex-col mobile:gap-[5px] mobile:text-[12px] md:text-[14px] lg:text-[16px] text-gray-6E">
+          <div className="flex justify-between flex-wrap gap-x-2 gap-y-2 mobile:flex-col mobile:gap-[5px] mobile:text-[12px] md:text-[14px] lg:text-[16px] text-gray-6E">
             <div className="flex items-center gap-[15px]">
-              <p>리뷰 {reviewCount}</p>
-              <p>찜 {favoriteCount}</p>
+              <p>리뷰 {formatCount(reviewCount)}</p>
+              <p>찜 {formatCount(favoriteCount)}</p>
             </div>
 
             <div className="flex items-center gap-[2px]">

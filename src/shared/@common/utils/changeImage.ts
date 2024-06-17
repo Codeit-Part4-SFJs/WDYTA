b30bb4b4df085807fac1fe4450b49e92f3d87fa4
yeona <aya813@naver.com>
2024-06-17 21:02:18 +0900

    #124 feat: handleImageChange 함수 공통에서 관리

---
+++ src/shared/@common/utils/changeImage.ts
@@ -0,0 +1,24 @@
+interface ImageChangeProps {
+  event: React.ChangeEvent<HTMLInputElement>;
+  setFile: React.Dispatch<React.SetStateAction<File | null>>;
+  setPreview: React.Dispatch<React.SetStateAction<string>>;
+}
+
+export const handleImageChange = ({
+  event,
+  setFile,
+  setPreview,
+}: ImageChangeProps) => {
+  const selectedFile = event.target.files?.[0] || null;
+  setFile(selectedFile);
+
+  if (selectedFile) {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      setPreview(reader.result as string);
+    };
+    reader.readAsDataURL(selectedFile);
+  } else {
+    setPreview('');
+  }
+};

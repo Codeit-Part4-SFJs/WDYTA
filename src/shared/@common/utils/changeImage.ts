a078765f1528e9abfa9b32ae4f80bbb29a1aa321
yeona <aya813@naver.com>
2024-06-18 13:16:38 +0900

    #124 feat: 파일 용량, 이름 바꾸기

--- src/shared/@common/utils/changeImage.ts
+++ src/shared/@common/utils/changeImage.ts
@@ -4,15 +4,28 @@ interface ImageChangeProps {
   setPreview: React.Dispatch<React.SetStateAction<string>>;
 }
 
+const MAX_SIZE = 5 * 1024 * 1024;
+
 export const handleImageChange = ({
   event,
   setFile,
   setPreview,
 }: ImageChangeProps) => {
   const selectedFile = event.target.files?.[0] || null;
-  setFile(selectedFile);
+  // const fileName = selectedFile.name;
+
+  if (selectedFile && selectedFile.size > MAX_SIZE) {
+    alert('이미지 파일의 최대 용량은 5MB입니다.');
+    return;
+  }
 
   if (selectedFile) {
+    const newFileName = Math.random().toString(36).substring(2, 8);
+    const newFile = new File([selectedFile], newFileName, {
+      type: selectedFile.type,
+    });
+    setFile(newFile);
+
     const reader = new FileReader();
     reader.onloadend = () => {
       setPreview(reader.result as string);

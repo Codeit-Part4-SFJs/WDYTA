9daf9e23dabe35d35da7c73fc501d774639c448b
DESKTOP-142GQLG\82106 <mangang0713@gmail.com>
2024-06-20 00:32:18 +0900

    #132 feat: 모달 기초 구현

---
+++ src/app/@modal/(.)modal/compare/page.tsx
@@ -0,0 +1,11 @@
+import { Modal } from '@/shared/ui/Modal';
+
+const CompareChangeModal = () => {
+  return (
+    <Modal size="large" closeIcon>
+      아
+    </Modal>
+  );
+};
+
+export default CompareChangeModal;
---
+++ src/components/@common/modal/CompareModal.tsx
@@ -0,0 +1,11 @@
+import { Modal } from '@/shared/ui/Modal';
+
+export const CompareModal = () => {
+  return (
+    <Modal size="large" closeIcon>
+      <div className="mobile:w-[335px] md:w-[590px] lg:w-[620px] flex justify-center items-center">
+        예시 1
+      </div>
+    </Modal>
+  );
+};

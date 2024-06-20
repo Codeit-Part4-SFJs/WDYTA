1d40d28a779371ff6c17fbc2a0e7e442693eb545
hyunjun9788 <namhyunjun9788@gmail.com>
2024-06-21 02:05:47 +0900

    #123 chore: 로그인 알림 모달 함수명 변경

--- src/app/modal/profile/page.tsx
+++ src/app/modal/profile/page.tsx
@@ -1,16 +1,12 @@
 import { AlertModal } from '@/components/@common/modal';
 import { Modal } from '@/shared/ui/Modal';
 
-const LoginAlarm = () => {
+const LoginAlert = () => {
   return (
     <Modal size="small" closeIcon={false}>
-      <AlertModal
-        errorMessage="로그인 후 이용해 주세요."
-        buttonText="닫기"
-        path="/login"
-      />
+      <AlertModal errorMessage="로그인 후 이용해 주세요." buttonText="닫기" />
     </Modal>
   );
 };
 
-export default LoginAlarm;
+export default LoginAlert;

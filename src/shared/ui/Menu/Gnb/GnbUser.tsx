7023acbdaecaf42391af0a53ec89f93d6f56e905
DESKTOP-142GQLG\82106 <mangang0713@gmail.com>
2024-06-20 20:04:21 +0900

    #88 fix: GnB에 사용되는 비교하기 위치 설정

--- src/shared/ui/Menu/Gnb/GnbUser.tsx
+++ src/shared/ui/Menu/Gnb/GnbUser.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { GnbSettingButton } from '@/shared/ui/Menu/Gnb/GnbSettingButton';
 import { GnbUserProps } from '@/shared/ui/Menu/Gnb/types/gnbType';
+import { useCompareItems } from '@/stores/useCompareItems';
 
 const flexClass = 'mobile:hidden md:flex lg:flex md:gap-[30px] lg:gap-[60px] ';
 const LinkClass =
@@ -20,9 +21,14 @@ const GnbUserSign = () => {
 };
 
 const GnbUserFeature = () => {
+  const firstItem = useCompareItems((state) => state.firstItem);
+  const secondItem = useCompareItems((state) => state.secondItem);
   return (
     <div className={flexClass}>
-      <Link className={LinkClass} href="/compare">
+      <Link
+        className={LinkClass}
+        href={`/compare?product1=${firstItem}&product2=${secondItem}`}
+      >
         비교하기
       </Link>
       <GnbSettingButton />

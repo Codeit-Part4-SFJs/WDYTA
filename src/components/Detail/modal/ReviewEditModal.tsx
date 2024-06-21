f9b17fe4bdeb34cd298f98238cbe78c515c662de
Sumin <csmxx99@naver.com>
2024-06-22 03:26:24 +0900

    feat: 리뷰 수정하기 모달 연결

---
+++ src/components/Detail/modal/ReviewEditModal.tsx
@@ -0,0 +1,212 @@
+'use client';
+
+import { useImageMutation } from '@/shared/@common/hooks';
+import { FormValues } from '@/shared/@common/types/input';
+import { Button, ButtonKind } from '@/shared/ui/Button/Button';
+import { CategoryChip } from '@/shared/ui/Chip/CategoryChip';
+import { Icon } from '@/shared/ui/Icon';
+import HelperText from '@/shared/ui/Input/HelperText';
+import { TextBoxInput } from '@/shared/ui/Input/TextBox';
+import { ImageInput } from '@/shared/ui/Input/image';
+import { useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useQueryClient } from '@tanstack/react-query';
+import { ALLOWED_TYPES } from '@/shared/@common/utils/constants/Image';
+import { Image } from '@/shared/@common/apis';
+import { useEditReviewMutation } from '../ProductReviews/hooks';
+
+interface ReviewEditModalProps {
+  accessToken: string;
+}
+
+const MAX_SIZE = 5 * 1024 * 1024;
+
+export const ReviewEditModal = ({ accessToken }: ReviewEditModalProps) => {
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<FormValues>({
+    mode: 'onChange',
+  });
+
+  const params = useSearchParams();
+  const productId = parseInt(params?.get('product') as string, 10) || 0;
+  const prevRating = parseInt(params?.get('rating') as string, 10) || 0;
+  const reviewId = parseInt(params?.get('reviewId') as string, 10) || 0;
+  const categoryId = parseInt(params?.get('category') as string, 10);
+  const productName = params?.get('name') || '';
+  const currentFilter = params?.get('filter') || '';
+
+  const [rating, setRating] = useState(prevRating);
+  const [files, setFiles] = useState<File[]>([]);
+  const [previews, setPreviews] = useState<string[]>([]);
+  const [errorMessage, setErrorMessage] = useState('');
+  const [isPending, setIsPending] = useState(false);
+
+  const ratingColors = Array(5)
+    .fill('fill-gray-6E')
+    .map((color, index) => (rating > index ? 'fill-yellow' : 'fill-gray-6E'));
+
+  const handleStarClick = (index: number) => {
+    setRating(index);
+  };
+
+  const text = watch('textarea', '');
+
+  const queryClient = useQueryClient();
+
+  const imageMutation = useImageMutation({ accessToken, setErrorMessage });
+  const editReviewMutation = useEditReviewMutation({
+    accessToken,
+    setErrorMessage,
+    queryClient,
+    productId,
+    currentFilter,
+    reviewId,
+  });
+
+  const isDisabled =
+    rating === 0 || !text || files.length === 0 || isSubmitting || isPending;
+
+  const handleDeleteButton = (index: number) => {
+    setPreviews((prevPreviews) => prevPreviews.filter((_, i) => i !== index));
+    setFiles((prevFiles) => prevFiles.filter((_, i) => i !== index));
+  };
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    if (event.target.files && event.target.files.length > 0) {
+      const newFiles = Array.from(event.target.files).slice(
+        0,
+        3 - files.length,
+      );
+      const validFiles = newFiles
+        .filter((file) => {
+          if (file.size > MAX_SIZE) {
+            setErrorMessage('이미지 파일의 최대 용량은 5MB입니다.');
+            return false;
+          }
+
+          if (!ALLOWED_TYPES.includes(file.type)) {
+            setErrorMessage(
+              '허용된 파일 형식은 .jpg, .jpeg, .png, .webp 입니다.',
+            );
+            return false;
+          }
+
+          return true;
+        })
+        .map((file) => {
+          const newFileName = Math.random().toString(36).substring(2, 8);
+          return new File([file], newFileName, { type: file.type });
+        });
+
+      if (validFiles.length > 0) {
+        setFiles((prevFiles) => [...prevFiles, ...validFiles]);
+
+        const newPreviews = validFiles.map((file) => URL.createObjectURL(file));
+        setPreviews((prevPreviews) => [...prevPreviews, ...newPreviews]);
+      }
+    }
+  };
+
+  const convertImageArray = (urls: string[]) => {
+    const newImages: Image[] = [];
+    urls.forEach((url) => {
+      newImages.push({ source: url });
+    });
+
+    return newImages;
+  };
+
+  const onSubmit: SubmitHandler<FormValues> = async () => {
+    setIsPending(true);
+
+    const promises = files.map((file) => {
+      return new Promise<string>((resolve, reject) => {
+        const formData = new FormData();
+        formData.append('image', file);
+
+        imageMutation
+          .mutateAsync(file)
+          .then((data) => resolve(data.url))
+          .catch(() => reject(new Error('Image upload failed')));
+      });
+    });
+
+    try {
+      const urls = await Promise.all(promises);
+      editReviewMutation.mutate({
+        images: convertImageArray(urls),
+        content: text,
+        rating,
+      });
+    } catch (error) {
+      setErrorMessage('이미지 업로드 중 오류가 발생했습니다');
+    }
+  };
+
+  return (
+    <form
+      className="flex flex-col gap-[40px] mobile:gap-5"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div className="flex flex-col gap-[10px]">
+        <CategoryChip categoryID={categoryId} />
+        <span className="text-gray-F1 text-xl lg:text-2xl font-semibold lg:leading-none">
+          {productName}
+        </span>
+      </div>
+      <div className="flex flex-col gap-[10px] md:gap-[15px] lg:gap-5">
+        <div className="flex items-center gap-[15px] lg:gap-5">
+          <p className="text-gray-6E text-sm lg:text-base">별점</p>
+          <div
+            className="flex gap-[2px] lg:gap-[5px]"
+            role="slider"
+            tabIndex={0}
+            aria-valuenow={rating}
+            aria-valuemin={1}
+            aria-valuemax={5}
+          >
+            {ratingColors.map((color, index) => (
+              <Icon
+                key={Math.random()}
+                name="StarIcon"
+                className={`w-8 h-8 mobile:w-7 mobile:h-7 ${color}`}
+                onClick={() => handleStarClick(index + 1)}
+              />
+            ))}
+          </div>
+        </div>
+        <TextBoxInput
+          register={register}
+          text={text}
+          placeholder="리뷰를 작성해 주세요"
+        />
+        <div className="flex mobile:w-[315px] gap-[10px] md:gap-[15px] lg:gap-5 overflow-x-auto scrollbar-hide">
+          {previews.length < 3 && (
+            <ImageInput previewImage="" handleImageChange={handleImageChange} />
+          )}
+          {previews.map((preview, index) => (
+            <ImageInput
+              key={Math.random()}
+              previewImage={preview}
+              handleDeleteButton={() => handleDeleteButton(index)}
+            />
+          ))}
+        </div>
+        {errorMessage && <HelperText type="error">{errorMessage}</HelperText>}
+      </div>
+      <Button
+        type="submit"
+        kind={ButtonKind.primary}
+        customSize="lg:text-lg w-[295px] md:w-[510px] lg:w-[540px] h-[50px] md:h-[55px] lg:h-[65px]"
+        disabled={isDisabled}
+      >
+        작성하기
+      </Button>
+    </form>
+  );
+};

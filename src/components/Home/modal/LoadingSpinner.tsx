279dd3150ec96085f739b83b98f254db1bacb381
linason-esc <linason7889@gmail.com>
2024-06-22 12:14:18 +0900

    [#167] 로딩 스피너 주석 제거

--- src/components/Home/modal/LoadingSpinner.tsx
+++ src/components/Home/modal/LoadingSpinner.tsx
@@ -3,7 +3,6 @@ import Image from 'next/image';
 export const LoadingSpinner = () => {
   return (
     <div className="z-[1000] fixed inset-0 flex justify-center items-center w-full h-full">
-      {/* <div className="animate-spin rounded-full h-20 w-20 border-t-2 border-b-2 border-main-blue" /> */}
       <Image
         src="/loadingSpinnerCircle.gif"
         alt="loadingSpinner"

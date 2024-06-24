14e32abc91cb23efa44c5dd846578a76bb04ab0c
Sumin <csmxx99@naver.com>
2024-06-25 03:04:18 +0900

    feat: modal 비로그인 시 리다이렉션

--- src/app/modal/detail/reviewEdit/page.tsx
+++ src/app/modal/detail/reviewEdit/page.tsx
@@ -1,9 +1,14 @@
 import { ReviewEditModal } from '@/components/Detail/modal';
 import { Modal } from '@/shared/ui/Modal';
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 const ProductReviewEditModal = () => {
   const accessToken = cookies().get('accessToken')?.value ?? '';
+
+  if (!accessToken) {
+    redirect('/modal/common/loginAlert');
+  }
 
   return (
     <Modal size="medium" closeIcon>
--- src/app/modal/home/productAdd/page.tsx
+++ src/app/modal/home/productAdd/page.tsx
@@ -1,9 +1,15 @@
 import { Modal } from '@/shared/ui/Modal';
 import { AddModal } from '@/components/Home/modal/AddModal';
 import { cookies } from 'next/headers';
+import { redirect } from 'next/navigation';
 
 const ProductAddModal = () => {
   const accessToken = cookies().get('accessToken')?.value ?? '';
+
+  if (!accessToken) {
+    redirect('/modal/common/loginAlert');
+  }
+
   return (
     <Modal size="medium" closeIcon>
       <AddModal accessToken={accessToken} title="상품 추가" />
